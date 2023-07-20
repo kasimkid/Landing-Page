1552513f2ced5ef7b1bccd3e63ccb1194b573c7b
Rafael Araujo <kasim.85.ra@gmail.com>
2023-07-19 20:16:21 -0400

    landin page test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./componentes/navbar";
+import { Card } from "./componentes/card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <div className="container-fluid">
+        <div className="row">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </>
   );
 }
 
